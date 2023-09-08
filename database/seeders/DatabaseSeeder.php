2166fb7b7736e3bd7c74bdede473f0d2e8547f10
SebastianParejaJimenez <110253953+SebastianParejaJimenez@users.noreply.github.com>
2023-09-08 16:21:12 -0500

    Fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(20)->create();
 
         DB::table('rols')->insert([
             'rol'  => 'Administrador',
--- resources/views/components/app/headerstore.blade.php
+++ resources/views/components/app/headerstore.blade.php
@@ -1,62 +1,127 @@
 <header>
-    <nav class=" top-0 bg-white dark:bg-[#182235] border-b border-slate-200 dark:border-slate-700 z-30 px-4 lg:px-6 py-2.5">
-        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="{{route('tienda')}}" class="flex items-center">
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">esClothing</span>
-            </a>
-            <div class="flex items-center lg:order-2">
-
-                <button  @click="cartOpen = !cartOpen" class="px-4 bg-white">
-                    <div class="relative scale-75">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-8 w-8 text-gray-600">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                      </svg>
-                      @if (Cart::content()->count())
-                      <span class="absolute -top-2 left-4 rounded-full bg-red-500 p-0.5 px-2 text-sm text-red-50">
-                        {{Cart::content()->count()}}
-                      </span>
-                      @else
-
-                      @endif
-
+    <section class="relative bg-gray-50 border-b font-poppins dark:bg-gray-800 dark:border-gray-800">
+        <div class="container mx-auto py-5" x-data="{ open: false }">
+            <nav class="flex justify-between ">
+                <div class="flex items-center justify-between w-full px-4 py-2 lg:px-2 ">
+                    <a href="{{route('tienda')}}" class="text-2xl text-gray-700 dark:text-gray-400">esClothing.</a>
+                    <div class="flex items-center lg:hidden ">
+                        <a href="" class="mr-4 dark:text-gray-400 hover:text-indigo-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                class="bi bi-heart" viewBox="0 0 16 16">
+                                <path
+                                    d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+                            </svg>
+                        </a>
+                        <button @click="cartOpen = !cartOpen" class="flex items-center mr-4 dark:text-gray-400 hover:text-indigo-500">
+                            <div class="relative mr-5">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                    fill="currentColor" class=" bi bi-cart" viewBox="0 0 16 16">
+                                    <path
+                                        d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                                </svg>
+                                @if (Cart::content()->count())
+                                    <span
+                                        class="absolute -top-2 left-4 rounded-full bg-red-500 p-0.5 px-2 text-sm text-red-50">
+                                        {{ Cart::content()->count() }}
+                                    </span>
+                                @else
+                                @endif
+                            </div>
+                        </button>
+                        <button
+                            class="flex items-center px-3 py-2 text-blue-600 border border-blue-200 rounded dark:text-gray-400 hover:text-blue-800 hover:border-blue-300 lg:hidden"
+                            @click="open =true">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-list" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                    d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                            </svg>
+                        </button>
                     </div>
-                  </button>
+                    <ul class="hidden font-medium lg:flex">
+                        @if (Auth::user() && Auth::user()->rol_id == 1)
+                            <li class="mr-12"><a href="{{route('dashboard')}}"
+                                    class="text-gray-700 hover:text-indigo-500 dark:text-gray-400">Dashboard</a>
+                            </li>
+                        @endif
+                        <li class="mr-12"><a href="{{route('pedidos_hechos')}}"
+                                class="text-gray-700 hover:text-indigo-500 dark:text-gray-400">Historial de Pedidos</a>
+                        </li>
+                        <li class="mr-12"><a href="{{route('catalogo')}}"
+                                class="text-gray-700 hover:text-indigo-500 dark:text-gray-400">Catalogo</a>
+                        </li>
 
-                @if (Auth::user())
-                <x-dropdown-profile align="right" />
+                    </ul>
+                    <div class="items-center justify-end hidden lg:flex dark:text-gray-400">
+                        <a href="" class="mr-7 hover:text-indigo-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor"
+                                class="bi bi-heart" viewBox="0 0 16 16">
+                                <path
+                                    d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z">
+                                </path>
+                            </svg>
+                        </a>
+                        <button @click="cartOpen = !cartOpen" class="flex items-center mr-6 dark:text-gray-400 hover:text-indigo-500">
+                            <div class="relative ">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor"
+                                    class="bi bi-cart" viewBox="0 0 16 16">
+                                    <path
+                                        d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                                </svg>
+                                @if (Cart::content()->count())
+                                <span
+                                    class="absolute -top-2 left-4 rounded-full bg-red-500  px-2 text-sm text-red-50">
+                                    {{ Cart::content()->count() }}
+                                </span>
+                                @else
+                                @endif
+                            </div>
+                        </button>
+                        @if (Auth::user())
+                            <x-dropdown-profile align="right" />
+                        @else
+                            <a href="{{ route('login') }}"
+                                class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Inicia
+                                Sesión</a>
+                        @endif
+                    </div>
+                </div>
+            </nav>
+            <!-- Mobile Sidebar -->
+            <div class="fixed inset-0 w-full bg-gray-800 opacity-25 lg:hidden dark:bg-gray-400"
+                :class="{ 'translate-x-0 ease-in-opacity-100': open === true, '-translate-x-full ease-out opacity-0': open ===
+                        false }">
+            </div>
+            <div class="fixed inset-0 z-10 h-screen p-3 text-gray-700 duration-500 transform shadow-md dark:bg-gray-800 bg-blue-50 w-80 lg:hidden lg:transform-none lg:relative"
+                :class="{ 'translate-x-0 ease-in-opacity-100': open === true, '-translate-x-full ease-out opacity-0': open ===
+                        false }">
+                <div class="flex justify-between">
+                    <a class="p-2 text-2xl font-bold dark:text-gray-400" href="#">esClothing.</a>
+                    <button class="p-2 rounded-md hover:text-blue-300 lg:hidden dark:text-gray-300" @click="open=false">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                            class="bi bi-x-circle" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                            <path
+                                d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                        </svg>
+                    </button>
+                </div>
 
-                @else
-                <a href="{{route('login')}}" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Inicia Sesión</a>
-                @endif
-            </div>
-            <div class="flex sm:hidden">
-                <button @click="isOpen = !isOpen" type="button" class="text-gray-600 hover:text-gray-500 focus:outline-none focus:text-gray-500" aria-label="toggle menu">
-                    <svg viewBox="0 0 24 24" class="h-6 w-6 fill-current">
-                        <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
-                    </svg>
-                </button>
-            </div>
-        <nav :class="isOpen ? '' : 'hidden'" class="sm:flex sm:justify-center sm:items-center mt-4">
-            <div class=" justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="">
-                <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                    <li>
-                        <a href="{{url('/tienda')}}" class="block py-2 pr-4 pl-3 text-gray-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Inicio</a>
-                    </li>
+                <ul class="px-5 text-left mt-7">
                     @if (Auth::user() && Auth::user()->rol_id == 1)
-                    <li>
-                        <a href="{{url('/dashboard')}}" class="block py-2 pr-4 pl-3 text-gray-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Dashboard</a>
-                    </li>
+                        <li class="pb-3">
+                            <a href="{{route('dashboard')}}" class="text-sm text-gray-700 hover:text-blue-400 dark:text-gray-100">Dashboard</a>
+                        </li>
                     @endif
-                    <li>
-                        <a href="{{route('pedidos_hechos')}}" class="block py-2 pr-4 pl-3 text-gray-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Historial de Pedidos</a>
+                    <li class="pb-3">
+                        <a href="{{route('catalogo')}}" class="text-sm text-gray-700 hover:text-blue-400 dark:text-gray-400">Catalogo</a>
                     </li>
-                    <li>
-                        <a href="{{route('catalogo')}}" class="block py-2 pr-4 pl-3 text-gray-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Catálogo</a>
+                    <li class="pb-3">
+                        <a href="{{route('pedidos_hechos')}}"
+                            class="text-sm text-gray-700 hover:text-blue-400 dark:text-gray-400">Historial de Pedidos</a>
                     </li>
                 </ul>
             </div>
-        </nav>
-
         </div>
-    </nav>
+    </section>
 </header>
